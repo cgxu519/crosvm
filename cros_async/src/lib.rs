49153460cbbe3a8495cedb5dd116751a8cf8c4da
Dylan Reid <dgreid@chromium.org>
2019-11-07 13:46:13 -0800

    combo
    Change-Id: If6f16a578e85b5cef75161f1666f332c9c5bdb0f

--- cros_async/src/lib.rs
+++ cros_async/src/lib.rs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+mod combos;
 mod fd_executor;
 
+pub use combos::await_two;
 pub use fd_executor::{add_future, add_waker, FdExecutor};
