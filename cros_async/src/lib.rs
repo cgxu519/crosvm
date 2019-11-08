ac6a2e079ff17a09283291680dc481b97c73f625
Dylan Reid <dgreid@chromium.org>
2019-11-08 11:30:28 -0800

    async: Add asynchronous read ability
    Change-Id: I292ce0a209bd5b1e75062ce063964b1c65c6f11a

--- cros_async/src/lib.rs
+++ cros_async/src/lib.rs
@@ -2,8 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+mod async_ops;
 mod combos;
 mod fd_executor;
 
+pub use async_ops::{AwaitRead, ReadAsync};
 pub use combos::await_two;
 pub use fd_executor::{add_future, add_waker, FdExecutor};
